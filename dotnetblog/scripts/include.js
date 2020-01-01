8b23bd937c7187c2c2e7bec387069873b6152eb5
casktCase <44332932+casktCase@users.noreply.github.com>
2019-12-31 23:48:12 -0700

    fixed tabs
    fixed tabs included via js

---
+++ dotnetblog/scripts/include.js
@@ -0,0 +1,7 @@
+
+function includeHTML() {
+  document.write('<ul class="basictab">');
+  document.write('<li><a href="index.html" title="Hits"><span>home</span></a></li>');
+  document.write('<li><a href="gallery.html" title="Hits"><span>gallery</span></a></li>');
+  document.write('</ul>');
+}
